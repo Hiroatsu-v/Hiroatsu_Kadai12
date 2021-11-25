2b899739d83a2e1e110117602e59a31ee6c588cc
Hiroatsu <hiroatsu.7190@gmail.com>
2021-11-25 17:20:26 +0900

    フィードバックを受けて修正

--- Kadai12/CalculateTax.swift.swift
+++ Kadai12/CalculateTax.swift.swift
@@ -9,14 +9,21 @@ import Foundation
 
 // 計算するクラス
 class CalculateTax {
-    private var price: Double
-    private var tax: Double
+//    private var price: Double
+//    private var tax: Double
+//
+//    init(price: Double, tax: Double) {
+//        self.price = price
+//        self.tax = tax / 100 + 1
+//    }
+//    func calculator() -> Double {
+//        price * tax
+//    }
 
-    init(price: Double, tax: Double) {
-        self.price = price
-        self.tax = tax / 100 + 1
-    }
-    func calculator() -> Double {
-        price * tax
+    // イニシャライザに渡す形だと計算するたびにインスタンス生成する必要がある。（上記の形) / DIなどで困ることがある。
+
+    // メソッドの引数として渡すほうがよい
+    func calculator(price: Double, tax: Double) -> Double {
+        price * (tax / 100 + 1)
     }
 }
